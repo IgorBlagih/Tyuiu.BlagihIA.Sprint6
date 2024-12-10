f2bed6a2346e5639e7b0f9cd6e930f5fade9b3b8
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-12-10 21:35:12 +0500

    Task3

--- Tyuiu.BlagihIA.Sprint6.Task3.V11.Lib/DataService.cs
+++ Tyuiu.BlagihIA.Sprint6.Task3.V11.Lib/DataService.cs
@@ -5,17 +5,18 @@ namespace Tyuiu.BlagihIA.Sprint6.Task3.V11.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < 1; j++)
                 {
                     for (var k = 0; k < matrix.GetLength(1); k++)
                     {
-                        if (matrix[i, j] <= matrix[i, k])
+
+                        if (matrix[i, j] <= matrix[k, j])
                         {
                             var temp = matrix[i, j];
-                            matrix[i, j] = matrix[i, k];
-                            matrix[i, k] = temp;
+                            matrix[i, j] = matrix[k, j];
+                            matrix[k, j] = temp;
                         }
                     }
                 }
