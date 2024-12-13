419d01939b7ae844e053789833086f8b89983ad4
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-12-13 22:21:48 +0500

    Task7

---
+++ Tyuiu.BlagihIA.Sprint6.Task7.V24.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.BlagihIA.Sprint6.Task7.V24.Lib;
+namespace Tyuiu.BlagihIA.Sprint6.Task7.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
